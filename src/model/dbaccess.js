5e73d7ef9b084a8c6a4c7e145b32a0e2bb11b57d
Volker Brömmel <wbommel@users.noreply.github.com>
2019-04-10 11:32:14 +0200

    added rules by config.json

--- src/model/dbaccess.js
+++ src/model/dbaccess.js
@@ -1,35 +1,40 @@
 /**
-* Database format descriptions
-*
-* From / To format:
-*  12345678901234567890
-*  YYYY;MM;DD;hh;mm
-*
-* Example: Every day (depending on Weekdays field) from 08:00 to 11:00
-*  From: "0000;01;01;08;00"
-*  To  : "9999;12;31;11;00"
-*
-*
-*
-* Weekdays format:
-*  ----------------------------------
-*  Weekday Su|Mo|Tu|We| |Th|Fr|Sa|---
-*  ----------------------------------
-*  Value    1| 2| 4| 8| |16|32|64|128
-*  ==================================
-*  Bitmask  1| 1| 1| 1| | 1| 1| 1|  0
-*  ----------------------------------
-*
-* Example: Every work day Mo-Fr
-*  0111 1100 = 62
-*
-* Example: Every day
-*  0111 1111 = 127
-*
-* Example: Weekends
-*  1000 0010 = 65
-*/
-
+ * Database format descriptions
+ *
+ * From / To format:
+ *  12345678901234567890
+ *  YYYY;MM;DD;hh;mm
+ *
+ * Example: Every date from 01.01.0000 to 31.12.9999
+ *          Every week day from 08:00 to 11:00
+ *  From    : "0000;01;01;08;00"
+ *  To      : "9999;12;31;11;00"
+ *  Weekdays: 127 (see below)
+ *
+ * Example: Every date from 01.01.0000 to 31.12.9999
+ *          Saturdays and Sundays from 09:00 to 11:00
+ *  From    : "0000;01;01;09;00"
+ *  To      : "9999;12;31;11;00"
+ *  Weekdays: 65 (see below)
+ *
+ * Weekdays format:
+ *  ---------------------------------
+ *  Weekday --|Sa|Fr|Th| |We|Tu|Mo|Su
+ *  ---------------------------------
+ *  Value  128|64|32|16| | 8| 4| 2| 1
+ *  =================================
+ *  Bitmask  0| 1| 1| 1| | 1| 1| 1| 1
+ *  ---------------------------------
+ *
+ * Example: Every work day Mo-Fr
+ *  0011 1110 = 62
+ *
+ * Example: Every day
+ *  0111 1111 = 127
+ *
+ * Example: Weekends
+ *  0100 0001 = 65
+ */
 
 
 /**
@@ -50,7 +55,7 @@ let mysql
  */
 let HOST = 'localhost';
 let PORT = 3306;
-let MYSQL_USER = 'root';    //your mysql/mariadb user. Do not use root. User your own user. ;-)
+let MYSQL_USER = 'root'; //your mysql/mariadb user. Do not use root. User your own user. ;-)
 let MYSQL_PASS = 'Warlock'; //your mysql/mariadb root pw. Do not use this example. Create your onw secure pw. ;-)
 let DATABASE = 'lightcontrol';
 let TABLE_RULES = 'lightrulesgeneral';
@@ -69,8 +74,8 @@ let isInitialized = false
 
 
 /**
-* main module object declaration
-*/
+ * main module object declaration
+ */
 module.exports = {
     /**
      * initialize datbase handler object
@@ -105,7 +110,10 @@ module.exports = {
      * reads db rules and gets the current rule if one applies now
      * @param {*} resultCallback function that will be executed when database query is done
      */
-    AnalyzeRules: function (resultCallback) { _analyzeRules(resultCallback); }
+    AnalyzeRules: function (resultCallback) {
+        //_analyzeRules(resultCallback)
+        _analyzeRulesFromConfig(resultCallback)
+    }
 }
 
 /**********************************************************************************************************************
@@ -122,12 +130,24 @@ module.exports = {
  * @param {*} dbRulesTable 
  */
 function _initDatabaseVars(dbHost, dbPort, dbUser, dbPass, dbName, dbRulesTable) {
-    if (dbHost) { HOST = dbHost }
-    if (dbPort) { PORT = dbPort }
-    if (dbUser) { MYSQL_USER = dbUser }
-    if (dbPass) { MYSQL_PASS = dbPass }
-    if (dbName) { DATABASE = dbName }
-    if (dbRulesTable) { TABLE_RULES = dbRulesTable }
+    if (dbHost) {
+        HOST = dbHost
+    }
+    if (dbPort) {
+        PORT = dbPort
+    }
+    if (dbUser) {
+        MYSQL_USER = dbUser
+    }
+    if (dbPass) {
+        MYSQL_PASS = dbPass
+    }
+    if (dbName) {
+        DATABASE = dbName
+    }
+    if (dbRulesTable) {
+        TABLE_RULES = dbRulesTable
+    }
 }
 
 /**
@@ -135,7 +155,9 @@ function _initDatabaseVars(dbHost, dbPort, dbUser, dbPass, dbName, dbRulesTable)
  * @param {*} resultCallback 
  */
 function _analyzeRules(resultCallback) {
-    if (!isInitialized) { throw NotInitializedException(this) }
+    if (!isInitialized) {
+        throw NotInitializedException(this)
+    }
 
     //create connection object
     let connection = mysql.createConnection({
@@ -160,8 +182,8 @@ function _analyzeRules(resultCallback) {
         if (err) {
             toLogger(util.format('dbaccess.js: %o', err), logger.LogLevelError)
         } else {
-            this.Rules = results;//TODO: not working since undefined, why?
-            rules = results;//TODO: not working since undefined, why?
+            this.Rules = results; //TODO: not working since undefined, why?
+            rules = results; //TODO: not working since undefined, why?
 
             for (let i in results) {
                 let rule = results[i];
@@ -178,13 +200,47 @@ function _analyzeRules(resultCallback) {
 
         if (!ruleFound) {
             if (typeof resultCallback === 'function') {
-                resultCallback(results, null);//return null if no rule is active
+                resultCallback(results, null); //return null if no rule is active
             }
         }
     });
 
     connection.end();
 }
+
+/**
+ * reads rules from config
+ * @param {*} resultCallback 
+ */
+function _analyzeRulesFromConfig(resultCallback) {
+    if (typeof resultCallback !== "function") {
+        return
+    }
+
+    if (!conf.rules) {
+        resultCallback(null, null, "ERROR no rules...")
+        return
+    }
+
+    let ruleFound = false
+    this.Rules = conf.rules
+    rules = conf.rules
+
+    for (let i in rules) {
+        let rule = rules[i]
+
+        if (_ruleApplies(rule)) {
+            ruleFound = true;
+            resultCallback(rules, rule);
+            break;
+        }
+    }
+
+    if (!ruleFound) {
+        resultCallback(rules, null); //return null if no rule is active
+    }
+}
+
 
 /**
  * checks with rule-validation module if rule applies now
@@ -207,4 +263,3 @@ function toLogger(message, level) {
         logger.LogIt(message, level)
     }
 }
-
