2cfda8cfefccedc92220b83bfe0e41982d1b2669
Volker Brömmel <wbommel@web.de>
2020-05-10 10:12:03 +0200

    activate change to master that was commited earlier, but never made it to production. the main change is moving away from mysql to config.json for reading lightrules.

---
+++ src/scripts/testrules.js
@@ -0,0 +1,28 @@
+const conf = require('../config.json')
+const TimeIsInRange = require('../model/rulevalidation').TimeIsInRange
+const TodayIsInRuleRange = require('../model/rulevalidation').TodayIsInRuleRange
+const TodayIsTheCorrectWeekday = require('../model/rulevalidation').TodayIsTheCorrectWeekday
+const YearIsInRuleRange = require('../model/rulevalidation').YearIsInRuleRange
+
+const filteredRules = []
+
+for (const i in conf.rules) {
+  const rule = conf.rules[i]
+
+  if (!YearIsInRuleRange(rule)) { continue }
+  if (!TodayIsInRuleRange(rule)) { continue }
+  if (!TodayIsTheCorrectWeekday(rule)) { continue }
+  if (!TimeIsInRange(rule)) { continue }
+
+  filteredRules.push(rule)
+}
+
+console.log('filteredRules.Count: %s', filteredRules.length)
+for (const i in filteredRules) {
+  const r = filteredRules[i]
+  console.log(r)
+}
+
+if (filteredRules.length > 1) {
+  console.log('We need to filter further...')
+}
